d35fe3c65fc1f95be0afd607dfe0dd11e6690e2e
mohammadamin <mdaminjilani313@gmail.com>
2025-04-29 16:46:32 +0530

    Isolated backend code for deployment

--- DB/connsection.js
+++ DB/connsection.js
@@ -1,5 +1,11 @@
 import { connect } from 'mongoose';
-const uri = 'mongodb+srv://mdaminjilani313:ZX8uJPmnnev2ucvn@chating.e1ddk.mongodb.net/?retryWrites=true&w=majority&appName=chating';
+import dotenv from 'dotenv';
+
+// Load environment variables from .env file
+dotenv.config();
+
+const uri = process.env.MONGO_URI;
+
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 export default connect(uri, {
   useNewUrlParser: true,
